7504fc5f7c48ccdc49cdc68f64f601cd43103aa9
limboy <limboy.me@gmail.com>
2017-09-04 23:28:22 +0800

    Add some adjustment to support carthage

--- DateToolsSwift/DateTools/Date+TimeAgo.swift
+++ DateToolsSwift/DateTools/Date+TimeAgo.swift
@@ -243,7 +243,8 @@ public extension Date {
         // However, a seemingly-equivalent method from NSBundle is: https://github.com/apple/swift-corelibs-foundation/blob/master/Foundation/NSBundle.swift
             return Bundle.main.localizedString(forKey: string, value: "", table: "DateTools")
         #else
-            return NSLocalizedString(string, tableName: "DateTools", bundle: Bundle.dateToolsBundle(), value: "", comment: "")
+            // return NSLocalizedString(string, tableName: "DateTools", bundle: Bundle.dateToolsBundle(), value: "", comment: "")
+            return string
         #endif
     }
     
